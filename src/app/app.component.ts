b63a3b08efbedda5dc59d4bea58d38e2be9a0cdd
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2022-03-17 11:51:51 -0300

    PrimeiroCommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bytebank';
+
+  transferencia : any;
+
+
+  transferir($event){
+    console.log($event);
+    this.transferencia = $event ;
+
+  }
+}
